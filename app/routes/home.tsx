d3df8bb9c69af3854c8bfce26f8200f1410bcc0d
ldimbe20 <ldimbe20@gmail.com>
2022-08-15 12:54:30 -0500

    Created kudo data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,7 +5,11 @@ import { requireUserId } from '~/utils/auth.server'
 import { Layout } from '~/components/layout'
 import { getOtherUsers } from '~/utils/user.server'
 import { UserPanel } from '~/components/user-panel'
-import { useLoaderData } from '@remix-run/react'
+import { useLoaderData,  Outlet
+} from '@remix-run/react'
+
+// This is the parent of any components in the home folder, 
+// Remix will recognize any files in the new home folder as sub-routes of /home
 
 
 
@@ -20,6 +24,7 @@ export default function Home() {
   const { users } = useLoaderData()
   return (
     <Layout>
+      <Outlet />
       <div className="h-full flex">
         <UserPanel users={users} />
         <div className="flex-1"></div>
